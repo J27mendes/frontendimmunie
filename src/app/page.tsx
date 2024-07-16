db32bfb1f0916f97ea6c01e3b2a0fb964268fea4
J27mendez <mendes_j27@hotmail.com>
2024-07-16 12:49:21 -0300

    feat: colors pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Form from "./components/form/Form";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-customColor">
       <Header />
       <Divider />
       <Form />
