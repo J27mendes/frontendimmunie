41d97a7a99b28cf5bd909e7d5be17a6b36ccfeda
J27mendez <mendes_j27@hotmail.com>
2024-07-27 08:29:28 -0300

    feat: stop showing the button

--- src/app/components/button/Button.tsx
+++ src/app/components/button/Button.tsx
@@ -1,8 +1,8 @@
 const Button = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="hidden justify-center items-center">
       <div className="border-2 border-black inline-block rounded-md">
-        <button className="flex justify-center items-center bg-green-600 border-2 border-white text-white font-bold py-2 px-4 rounded-md hover:bg-green-500 hover:border-green-300 hover:text-black">
+        <button className="hidden justify-center items-center bg-green-600 border-2 border-white text-white font-bold py-2 px-4 rounded-md hover:bg-green-500 hover:border-green-300 hover:text-black">
           Cadastrar
         </button>
       </div>
